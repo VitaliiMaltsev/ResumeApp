8cd23f7ef57f7e1a3c13eb890e613b8a3f8e919b
Vitaliy <ihbectop@gmail.com>
2018-06-07 00:47:46 +0300

    enchance contact types

--- src/com/company/ContactType.java
+++ src/com/company/ContactType.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    MAIL,
-    SKYPE
+    PHONE("Тел. "),
+    MOBILE("Мобильный тел. "),
+    HOME_PHONE("Домашний тел. "),
+    MAIL("e-mail: "),
+    SKYPE("Skype: ");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- src/com/company/SectionType.java
+++ src/com/company/SectionType.java
@@ -1,9 +1,23 @@
 package com.company;
 
 public enum SectionType {
-    OBJECTIVE,
-    ARCHIEVEMENT,
-    EXPIRIENCE,
-    QUALIFICATIONS,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ARCHIEVEMENT("Достижения"),
+    EXPIRIENCE("Опыт работы"),
+    QUALIFICATIONS("Квалификация"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
+
+
+
+
