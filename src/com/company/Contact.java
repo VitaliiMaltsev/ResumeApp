983d44353456b88fc94c306c7a8fa823b583a9f1
Vitaliy <ihbectop@gmail.com>
2018-06-06 21:27:44 +0300

    Add new classes

---
+++ src/com/company/Contact.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Contact {
+private final String type;
+private final String value;
+
+    public Contact(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
